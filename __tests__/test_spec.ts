3014a4da166f69d377a1c68d9f9559894bd897cf
ekologic <ekologic90@gmail.com>
2018-05-08 10:44:07 +0200

    tests refactored

--- __tests__/test_spec.ts
+++ __tests__/test_spec.ts
@@ -1,48 +1,52 @@
 describe('Challenge', function () {
   it('should parse numbers', function () {
-    expect(calculate('1 2 3') ===  3)
+    expect(calculate('1 2 3')).toBe(3)
   })
   it('should parse floats', function() {
-    expect(calculate('1 2 3.5') === 3.5)
+    expect(calculate('1 2 3.5')).toBe(3.5)
   })
   it('should support addition', function() {
-    expect(calculate('1 3 +') === 4)
+    expect(calculate('1 3 +')).toBe(4)
   })
   it('should support multiplication', function() {
-    expect(calculate('1 3 *') ===  3)
+    expect(calculate('1 3 *')).toBe(3)
   })
   it('should support subtraction', function() {
-    expect(calculate('1 3 -') === -2)
+    expect(calculate('1 3 -')).toBe(-2)
   })
   it('should support division', function() {
-    expect(calculate('4 2 /') === 2)
+    expect(calculate('4 2 /')).toBe(2)
   })
 
   it('should support evaluate this operation right', function() {
     expect(calculate('3 2 1 - *')).toBe(3)
   })
   it('should support evaluate this operation right', function() {
-    expect(calculate('7 4 5 + * 3 - 10 /') === 6)
+    expect(calculate('7 4 5 + * 3 - 10 /')).toBe(6)
   })
   it('should support evaluate this operation right', function() {
-    expect(calculate('') === 0)
+    expect(calculate('')).toBe(0)
   })
 })
 
 const calculate = (str: string) => {
   const items = str.split(' ');
-  if (items.length <= 3) {
+  if(items.length === 1) {
+    return 0;
+  }
+  if (items.length <= 2) {
     return items[items.length - 1];
   }
   
   const root:MyNode = buildNode(items.pop(),items);
-  return calculate1(root);
+  const result = Number(calculate1(root))
+  return Number.isNaN(result) ? 0 : result;
 }
 const operations = {
-  '+' : (a:number,b:number) => a + b,
-  '*' : (a:number,b:number) => a * b,
-  '-' : (a:number,b:number) => a - b,
-  '/' : (a:number,b:number) => a / b,
+  '+' : (a:number,b:number) => Number(a) + Number(b),
+  '*' : (a:number,b:number) => Number(a) * Number(b),
+  '-' : (a:number,b:number) => Number(a) - Number(b),
+  '/' : (a:number,b:number) => Number(a) / Number(b),
 }
 
 const buildNode = (operandOrNumber: any, array: string[]): MyNode => {
